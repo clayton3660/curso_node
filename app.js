eaf13832b0b0c70f7dd2b51cb9dc6ac4e68957a8
Clayton Silva <clayton3660@gmail.com>
2021-08-04 11:34:00 -0300

    modulo_1_introducao

---
+++ app.js
@@ -0,0 +1,25 @@
+var calculadora = require('./calculadora');
+
+console.log(calculadora.soma(10, 20));
+console.log(calculadora.multi(10, 20));
+console.log(calculadora.sub(20, 5));
+console.log(calculadora.div(10, 2));
+console.log(calculadora.nome);
+
+// console.log(somaFuncao(10, 20));
+// var resultado = multi(10, 10);
+// var resultadoSub = sub(20, 40);
+// console.log(div(10, 2));
+// console.log(resultado);
+// console.log(resultadoSub);
+
+// var mostrarSite = false;
+// const site = 'www.google.com';
+
+// console.log('Hello World!');
+// console.log('Meu nome é: ');
+// console.log('E estou aprendendo Node.js');
+
+// if (mostrarSite) {
+//   console.log(site);
+// }
---
+++ calculadora.js
@@ -0,0 +1,16 @@
+var nome = 'Minha calculadora V1';
+
+function soma(a, b) {
+  return a + b;
+}
+function multi(a, b) {
+  return a * b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function div(a, b) {
+  return a / b;
+}
+
+module.exports = { soma, multi, div, sub, nome };
