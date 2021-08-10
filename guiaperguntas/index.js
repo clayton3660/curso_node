7f01d017e1fa85128a00d07f9e90a48faef36f95
Clayton Silva <clayton3660@gmail.com>
2021-08-10 01:27:12 -0300

    apresentando_e_instalando_o_bootstrap

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -5,34 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
-
-  var produtos = [
-    { nome: 'Doritos', preco: 3.14 },
-    { nome: 'Coca-cola', preco: 5 },
-    { nome: 'Leite', preco: 1.45 },
-    { nome: 'Carne', preco: 15 },
-    { nome: 'Redbull', preco: 6 },
-    { nome: 'Nescau', preco: 4 },
-  ];
-
-  res.render('index', {
-    nome: nome,
-    lang: lang,
-    empresa: 'GateSix',
-    inscritos: 8000,
-    msg: exibirMsg,
-    produtos: produtos,
-  });
-});
-app.get('/home', (req, res) => {
-  res.render('home');
-});
-app.get('/perfil', (req, res) => {
-  res.render('principal/perfil');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(8080, () => {
