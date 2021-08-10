e719847709bc640dd79ef14f4a346006709ff325
Clayton Silva <clayton3660@gmail.com>
2021-08-10 00:43:35 -0300

    etruturas_de_repeticao_com_ejs

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -7,13 +7,24 @@ app.set('view engine', 'ejs');
 app.get('/:nome/:lang', (req, res) => {
   var nome = req.params.nome;
   var lang = req.params.lang;
-  var exibirMsg = true;
+  var exibirMsg = false;
+
+  var produtos = [
+    { nome: 'Doritos', preco: 3.14 },
+    { nome: 'Coca-cola', preco: 5 },
+    { nome: 'Leite', preco: 1.45 },
+    { nome: 'Carne', preco: 15 },
+    { nome: 'Redbull', preco: 6 },
+    { nome: 'Nescau', preco: 4 },
+  ];
+
   res.render('index', {
     nome: nome,
     lang: lang,
     empresa: 'GateSix',
     inscritos: 8000,
     msg: exibirMsg,
+    produtos: produtos,
   });
 });
 app.get('/home', (req, res) => {
