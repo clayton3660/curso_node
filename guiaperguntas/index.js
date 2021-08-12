18d3fe9c606c8aadf57d541f5c6253812a9975d7
Clayton Silva <clayton3660@gmail.com>
2021-08-12 01:07:14 -0300

    Salvando_perguntas_no_banco_de_dados

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -30,7 +30,12 @@ app.get('/perguntar', (req, res) => {
 app.post('/salvarpergunta', (req, res) => {
   var titulo = req.body.titulo;
   var descricao = req.body.descricao;
-  res.send(`Formulario recebido! titulo ${titulo} descricao ${descricao}`);
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    res.redirect('/');
+  });
 });
 
 app.listen(8080, () => {
