20f264f384cab1dd90891f944f1afdf5bd81d8a8
Clayton Silva <clayton3660@gmail.com>
2021-08-15 10:58:33 -0300

    Busca_condicional_com_sequelize

--- guiaperguntas/index.js
+++ guiaperguntas/index.js
@@ -48,6 +48,19 @@ app.post('/salvarpergunta', (req, res) => {
     res.redirect('/');
   });
 });
+app.get('/pergunta/:id', (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: { id: id },
+  }).then((pergunta) => {
+    if (pergunta != undefined) {
+      res.render('pergunta');
+    } else {
+      // não encontrada
+      res.redirect('/');
+    }
+  });
+});
 
 app.listen(8080, () => {
   console.log('App rodando!');
