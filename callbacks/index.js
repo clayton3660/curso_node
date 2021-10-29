1a6b37a834fc51e3f51f8640897e81a401235561
Clayton Silva <clayton3660@gmail.com>
2021-10-29 11:34:20 -0300

    Tratamento_de_erros_em_callback

--- callbacks/index.js
+++ callbacks/index.js
@@ -1,24 +1,23 @@
 function enviarEmail(corpo, para, callback) {
   setTimeout(() => {
-    console.log(`
-      Para: ${para}
-      --------------------------------------------------------
-      ${corpo}
-      --------------------------------------------------------
-      De: Clayton
-    `);
-    callback('Ok', 5, '8s', 'clayton'); // pode ser utilizados parametros
-  }, 8000);
+    
+    var deuErro = false;
+
+    if(deuErro) {
+      callback(12,'O envio do e-mail falhou');
+
+    }else {
+      callback(12); // pode ser utilizados parametros
+    }
+    
+  }, 2000);
 }
 console.log('Inicio do envio do e-mail');
-enviarEmail('Oi seja bem vindo ao curso de node', 'email@email.com', (status, amount, time, nome) => {
-  console.log(`
-    De: ${nome}
-    Status: ${status}
-    -----------------
-    Contatos: ${amount}
-    -----------------
-    Tempo: ${time}
-  `);
-  console.log('Tudo Ok');
+enviarEmail('Oi seja bem vindo ao curso de node', 'email@email.com', (time,erro) => {
+  if(erro == undefined) {
+    console.log('Tudo ok')
+    console.log(`Tempo: ${time}s `)
+  }else {
+    console.log(`Ocorreu um erro: ${erro}`)
+  }
 });
