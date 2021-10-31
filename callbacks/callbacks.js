6d036651a7afebd848e95ba899089fc8ae0ff2bc
Clayton Silva <clayton3660@gmail.com>
2021-10-31 00:43:57 -0300

    Promises_aninhadas_ou_Promise_Hell

--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -1,24 +1,38 @@
+function pegarId() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(5);
+    }, 1500);
+  });
+}
+
+function  buscarEmailNoBanco(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('email@email.com');
+    }, 2000);
+  });
+}
+
 function enviarEmail(corpo, para) {
   return new Promise((resolve, reject) => {
-    setTimeout(() =>{
-
+    setTimeout(() => {
       var deuErro = false;
       //console.log('Email enviado')
-      if(!deuErro) {
-        resolve({time: 6, to: "email@email.com"});
-      }else {
-        reject("Fila cheia")
+      if (!deuErro) {
+        resolve({ time: 6, to: 'email@email.com' });
+      } else {
+        reject('Fila cheia');
       }
-    }, 4000)
-  })
+    }, 4000);
+  });
 }
-enviarEmail("Olá mundo", 'email@email.com').then(({time, to}) =>{
-  console.log(`
-    Time: ${time}
-    ___________________
-    To: ${to}
-  `)
-
-}).catch((erro) =>{
-  console.log('Que pena, não deu! :(' + erro)
+pegarId().then ((id) => {
+  buscarEmailNoBanco(id).then((email) => {
+    enviarEmail('Olá, como vai?!', email ).then(() => {
+      console.log("Email enviado, para o usuário com id: " + id)
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 })
