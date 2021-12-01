e05546ae98f8a7345b7f66fee3c78ee7642554af
Clayton <clayton3660@gmail.com>
2021-12-01 11:10:20 -0300

    This

--- orientacao_a_objetos/Filme.js
+++ orientacao_a_objetos/Filme.js
@@ -23,20 +23,28 @@ class Filme {
   Fechar() {
     console.log('Fechar X');
   }
+  Ficha(){
+    console.log("Titulo: " + this.titulo)
+    console.log("Ano de lançamento: " + this.ano)
+    console.log("Genero: " + this.genero)   
+    console.log("Diretor: " + this.diretor)   
+    console.log("Duração: " + this.duracao)   
+    this.Reproduzir();
+  }
 }
 
 var vingadores = new Filme('Vingadores 2',2014,"Ação","Alguem","2h");
+vingadores.Ficha();
 
 
-
-console.log('Titulo do filme: ' + vingadores.titulo);
-console.log('Ano de lançamento: ' + vingadores.ano);
+// console.log('Titulo do filme: ' + vingadores.titulo);
+// console.log('Ano de lançamento: ' + vingadores.ano);
 
 var batman = new Filme("Batman",2009,"Ação","Alguem 2","2h30");
-
+batman.Ficha();
 // batman.titulo = "batman"
 // batman.ano = "2009"
 // batman.genero = "Ação"
 
-console.log(batman.titulo)
-console.log(batman.genero)
+// console.log(batman.titulo)
+// console.log(batman.genero)
